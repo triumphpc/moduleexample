55561df11f5a18a1edb8b6888e47a200528c9e8c
Sergey Vrulin <triumph.job@gmail.com>
2021-07-28 22:48:16 +0300

    add new module

---
+++ moduleexample.go
@@ -0,0 +1,9 @@
+// moduleexample show example for use modules
+package moduleexample
+
+import "fmt"
+
+// Hi example for output
+func Hi(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
