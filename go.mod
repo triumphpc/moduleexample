1842383b4261b818e379d0fd9870d98177062015
Sergey Vrulin <triumph.job@gmail.com>
2021-08-02 08:29:52 +0300

    Add version 2 for module

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/triumphpc/moduleexample
+module github.com/triumphpc/moduleexample/v2
 
 go 1.16
--- moduleexample.go
+++ moduleexample.go
@@ -1,9 +1,15 @@
 // moduleexample show example for use modules
 package moduleexample
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hi example for output
-func Hi(name string) string {
-	return fmt.Sprintf("Hello, %s!!!", name)
+func Hi(name, info string) (string, error) {
+	if info == "" {
+		return "", errors.New("unknown information")
+	}
+	return fmt.Sprintf("Hello, %s!!!, \nInformation for you:%s", name, info), nil
 }
